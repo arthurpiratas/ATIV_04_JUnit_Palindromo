2aa6bb997ede2ca7fa84ffb628ffe4cee33034fe
arthurpiratas <silva.arthursoares@gmail.com>
2019-09-08 17:14:24 -0300

    Implementação da Classe Palindromo

--- ATIV_04_JUnit_Palindromo/src/Negocio/Palindromo.java
+++ ATIV_04_JUnit_Palindromo/src/Negocio/Palindromo.java
@@ -4,8 +4,16 @@ public class Palindromo implements IPalindromo {
 
 	@Override
 	public boolean isPolindromo(String string) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		String string2 = new String();
+		
+		for (int i = string.length(); i > 0;  i++) {
+			if(string.charAt(i) != ' ') {
+				string2 += string.charAt(i);
+			}
+		}
+				
+		return string2.compareToIgnoreCase(string) == 0 ? true : false;
 	}
 
 }
